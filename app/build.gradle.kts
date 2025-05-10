87dc8a81e065c278c0141e997ca2beb93ff8d36e
Kerem Mert <kerem37mert@yahoo.com>
2025-05-10 11:14:51 +0300

    harita eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,11 +55,11 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation ("com.google.android.gms:play-services-maps:18.1.0")
     implementation ("com.google.android.material:material:1.12.0")
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
 }
--- app/src/main/java/com/example/kriptocep/AboutUsFragment.java
+++ app/src/main/java/com/example/kriptocep/AboutUsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,7 +19,9 @@ import android.view.ViewGroup;
 
 public class AboutUsFragment extends Fragment implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    MapView mapView;
+    Bundle mapViewBundle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,22 +33,67 @@ public class AboutUsFragment extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Google Map Fragment'ini al ve haritayı başlat
-        SupportMapFragment mapFragment = (SupportMapFragment)
-                getChildFragmentManager().findFragmentById(R.id.map);
+        mapView = view.findViewById(R.id.mapView);
 
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this); // onMapReady callback'ini bekliyoruz
-        }
+        if(savedInstanceState != null)
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-
         // Kastamonu Üniversitesi Mühendislik ve Mimarlık Fakültesi konumu
-        LatLng kastamonuMMF = new LatLng(41.3962, 33.7882);
-        mMap.addMarker(new MarkerOptions().position(kastamonuMMF).title("Kastamonu Üniversitesi MMF Fakültesi"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kastamonuMMF, 15)); // Zoom seviyesini belirle
+        LatLng kastamonuMMF = new LatLng(41.438152,33.763514);
+        googleMap.addMarker(new MarkerOptions().position(kastamonuMMF).title("Kastamonu Üniversitesi MMF"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kastamonuMMF, 15)); // Zoom seviyesini belirle
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle mapViewBundle = outState.getBundle("MAP_VIEW_BUNDLE_KEY");
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle("MAP_VIEW_BUNDLE_KEY", mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
     }
 }
