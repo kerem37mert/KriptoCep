4821b35f557f0fc6fef5bd1e1a321f92361a14c4
Kerem Mert <kerem37mert@yahoo.com>
2025-05-11 16:19:47 +0300

    apiye istek atarken süre kontrolü eklendi

--- app/src/main/java/com/example/kriptocep/CurrenciesAdapter.java
+++ app/src/main/java/com/example/kriptocep/CurrenciesAdapter.java
@@ -1,9 +1,7 @@
 package com.example.kriptocep;
 
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,14 +20,16 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrenciesAdapter.Vi
     List<Currencies> currencies;
 
     private String formatCurrency(double num) {
-        if (num >= 1000000000) {
-            return String.format("%.2fB", num / 1000000000);
-        } else if (num >= 1_000_000) {
-            return String.format("%.2fM", num / 1000000);
-        } else if (num >= 1_000) {
-            return String.format("%.2fK", num / 1000);
+        if (num >= 1_000_000_000_000.0) {
+            return String.format("%.2fT", num / 1_000_000_000_000.0);
+        } else if (num >= 1_000_000_000.0) {
+            return String.format("%.2fB", num / 1_000_000_000.0);
+        } else if (num >= 1_000_000.0) {
+            return String.format("%.2fM", num / 1_000_000.0);
+        } else if (num >= 1_000.0) {
+            return String.format("%.2fK", num / 1_000.0);
         } else {
-            return String.valueOf(num);
+            return String.format("%.2f", num);
         }
     }
 
@@ -47,11 +47,18 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrenciesAdapter.Vi
     @Override
     public void onBindViewHolder(@NonNull CurrenciesAdapter.ViewHolder holder, int position) {
         Currencies currency = currencies.get(position);
-        holder.coinNameTextView.setText(currency.name);
-        holder.coinPriceTextView.setText(String.valueOf(currency.price_usd));
-        holder.coinChangeTextView.setText(String.valueOf(currency.percent_change_24h));
+
+        // En fazla 6 karakterlik coin adı
+        String name = currency.name;
+        if (name.length() > 8) {
+            name = name.substring(0, 8)+ "...";
+        }
+        holder.coinNameTextView.setText(name);
+
+        holder.coinPriceTextView.setText(String.valueOf(currency.price_usd) + "$");
         holder.coinSymbolTextView.setText(currency.symbol);
 
+        // İkonlar
         try {
             String fileName = "icons/" + currency.symbol.toLowerCase() + ".png";
             InputStream inputStream = holder.itemView.getContext().getAssets().open(fileName);
@@ -63,8 +70,19 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrenciesAdapter.Vi
             holder.coinImageView.setImageResource(R.drawable.aboutus);
         }
 
+        // Market Değeri
         String formattedMarketCap = formatCurrency(currency.market_cap_usd);
-        holder.coinMarketCapTextView.setText(formattedMarketCap);
+        holder.coinMarketCapTextView.setText(formattedMarketCap + "$");
+
+        // Günlük Değişim
+        holder.coinChangeTextView.setText(String.valueOf(currency.percent_change_24h) + "%");
+
+        if(currency.percent_change_24h > 0)
+            holder.coinChangeTextView.setTextColor(holder.itemView.getResources().getColor(R.color.green));
+        else if(currency.percent_change_24h < 0)
+            holder.coinChangeTextView.setTextColor(holder.itemView.getResources().getColor(R.color.red));
+        else
+            holder.coinChangeTextView.setTextColor(holder.itemView.getResources().getColor(R.color.white));
     }
 
     @Override
@@ -78,12 +96,11 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrenciesAdapter.Vi
         TextView coinPriceTextView;
         TextView coinChangeTextView;
         TextView coinSymbolTextView;
-
         TextView coinMarketCapTextView;
         ImageView coinImageView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             coinMarketCapTextView = itemView.findViewById(R.id.coin_list_item_market_cap);
             coinNameTextView = itemView.findViewById(R.id.coin_list_item_name);
             coinPriceTextView = itemView.findViewById(R.id.coin_list_item_price);
