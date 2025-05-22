832eaa27551d91a5ebc49653d07b6c3451b0e389
Muhsin76 <muhsinkaraali76@gmail.com>
2025-05-22 12:58:29 +0300

    Bu seferde HAKKIMIZDA kısmına bişeyler ekledim

--- app/src/main/java/com/example/kriptocep/AboutUsFragment.java
+++ app/src/main/java/com/example/kriptocep/AboutUsFragment.java
@@ -1,99 +1,51 @@
 package com.example.kriptocep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class AboutUsFragment extends Fragment implements OnMapReadyCallback {
-
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
-    MapView mapView;
-    Bundle mapViewBundle;
+public class AboutUsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        Button adresBtn = view.findViewById(R.id.adresBtn);
+        Button hakkimizdaBtn = view.findViewById(R.id.hakkimizdaBtn);
+        Button iletisimBtn = view.findViewById(R.id.iletisimBtn);
 
-        mapView = view.findViewById(R.id.mapView);
+        adresBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), adres.class);
+                startActivity(intent);
+            }
+        });
 
-        if(savedInstanceState != null)
-            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        hakkimizdaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), hakkimizdaActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(this);
-    }
+        iletisimBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), iletisimActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        // Kastamonu Üniversitesi Mühendislik ve Mimarlık Fakültesi konumu
-        LatLng kastamonuMMF = new LatLng(41.438152,33.763514);
-        googleMap.addMarker(new MarkerOptions().position(kastamonuMMF).title("Kastamonu Üniversitesi MMF"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kastamonuMMF, 15)); // Zoom seviyesini belirle
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
-    public void onPause() {
-        mapView.onPause();
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        mapView.onStop();
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroy() {
-        mapView.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Bundle mapViewBundle = outState.getBundle("MAP_VIEW_BUNDLE_KEY");
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle("MAP_VIEW_BUNDLE_KEY", mapViewBundle);
-        }
-        mapView.onSaveInstanceState(mapViewBundle);
+        return view;
     }
 }
